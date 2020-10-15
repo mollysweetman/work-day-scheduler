dfe6a26de359158e48850700196c2e8cebedbad2
mollysweetman <67077842+mollysweetman@users.noreply.github.com>
2020-10-14 20:23:23 -0600

    setting variables

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 // adding variables
-var time = moment().format("h");
-
-$("#hour9").attr("data-time", moment("9:00 am", "h:mm a").format("h"));
-$("#hour10").attr("data-time", moment("10:00 am", "h:mm a").format("h"));
-$("#hour11").attr("data-time", moment("11:00 am", "h:mm a").format("h"));
-$("#hour12").attr("data-time", moment("12:00 pm", "h:mm a").format("h"));
-$("#hour1").attr("data-time", moment("1:00 pm", "h:mm a").format("h"));
-$("#hour2").attr("data-time", moment("2:00 pm", "h:mm a").format("h"));
-$("#hour3").attr("data-time", moment("3:00 pm", "h:mm a").format("h"));
-$("#hour4").attr("data-time", moment("4:00 pm", "h:mm a").format("h"));
-$("#hour5").attr("data-time", moment("5:00 pm", "h:mm a").format("h"));
-
 $(document).ready(function() {
 
+    var hour9 = 9;
+    var hour17 = 17; //5pm in 24 hour format
 
-})
+    var  planner = {};
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
 
